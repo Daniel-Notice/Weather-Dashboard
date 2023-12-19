95a6499b91eabc73ea2d5297fd01dd3338c3fd7a
DanielVNotice <92042221+DanielVNotice@users.noreply.github.com>
2023-12-19 07:22:32 +0000

    user is able to enter any city and get an output

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,37 +3,38 @@ let now = dayjs().format("DD/MM/YYYY");
 console.log(now);
 
 //selecting all of the elements
-const searchEL = document.querySelector(".weather-search");
-const searchELval = document.querySelector(".weather-search").value;
+const searchEL = document.querySelector("search-form input");
+const searchELval = document.querySelector(".weather-search");
 const searchBtn = document.querySelector("#search-button");
 const todaySection = document.querySelector("#today");
 const tempmain = document.querySelector(".temp-main");
 const windmain = document.querySelector(".wind-main");
 const humidity = document.querySelector(".humidity-main");
 const h2location = document.querySelector(".location");
-
-
-
+const leftside = document.querySelector(".left-side");
 
 //My api Key
 const APIKey = "d0b12f7162c21d128c6108524d623396";
-let queryURL = `https://api.openweathermap.org/data/2.5/forecast?units=metric&q=london&appid=${APIKey}`;
 
 let searchHistory = [];
 
-fetch(queryURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
+searchBtn.addEventListener("click", function (event) {
+  event.preventDefault(); // Prevent the form from submitting and refreshing the page
 
-    h2location.textContent = data.city.name + " " + "(" + now + ")";
-    tempmain.textContent = "Temp: " + data.list[0].main.temp + " °C";
-    windmain.textContent = "Wind: " + data.list[0].wind.speed + " KPH";
-    humidity.textContent = "Humidity: " + data.list[0].main.humidity + " %";
-  });
+  const cityName = searchELval.value;
+  let queryURL = `https://api.openweathermap.org/data/2.5/forecast?units=metric&q=${cityName}&appid=${APIKey}`;
+  console.log(cityName);
+  fetch(queryURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
 
-//   ${searchEL}
-
-searchBtn.addEventListener("submit", function () {});
+      h2location.textContent =
+        data.city.name + " " + "(" + now + ")" + data.list[0].weather[0].icon;
+      tempmain.textContent = "Temp: " + data.list[0].main.temp + " °C";
+      windmain.textContent = "Wind: " + data.list[0].wind.speed + " KPH";
+      humidity.textContent = "Humidity: " + data.list[0].main.humidity + " %";
+    });
+});
